e405d6c78aaee98ecca2f644e1a8f23f6eed205b
xvallot <xvallot@oxygene-tanukis.com>
2017-05-16 16:55:01 +0200

    win + npm versions compatibility

--- bin/dev361-front-builder.js
+++ bin/dev361-front-builder.js
@@ -3,44 +3,96 @@
 'use strict';
 var yargs = require("yargs");
 var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 var path = require('path');
 var cwd = path.join(__dirname, '../');
 var isWin = /^win/.test(process.platform);
 
 if (isWin) {
-  var gulpExecPath = path.join(__dirname, '../../.bin/gulp.cmd');
+  var gulpExecPathOld = path.join(__dirname, '../node_modules/.bin/gulp.cmd');
+  var gulpExecPathNew = path.join(__dirname, '../../.bin/gulp.cmd');
 } else {
-  var gulpExecPath = path.join(__dirname, '../../.bin/gulp');
+  var gulpExecPathOld = path.join(__dirname, '../node_modules/.bin/gulp');
+  var gulpExecPathNew = path.join(__dirname, '../../.bin/gulp');
 }
 
 var argv = yargs
     .command("build", "build everything for dev, no minimizing", function (yargs) {
-        spawn(
-            gulpExecPath,
-            ['build', '--cwd', cwd, '--color', 'always'],
-            { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
-        );
+        exec('npm -v', function (error, stdout, stderr) {
+            var versionNb = stdout.charAt(0);
+
+            if(versionNb < 3 ){
+                spawn(
+                    gulpExecPathOld,
+                    ['build', '--cwd', cwd, '--color', 'always'],
+                    { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
+                )
+            } else {
+                spawn(
+                    gulpExecPathNew,
+                    ['build', '--cwd', cwd, '--color', 'always'],
+                    { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
+                )
+            }
+        });
+        
     })
     .command("build:prod", "build task, but minimized", function (yargs) {
-        spawn(
-            gulpExecPath,
-            ['build:prod', '--cwd', cwd, '--color', 'always'],
-            { env: {NODE_ENV: 'prod'}, stdio: 'inherit' }
-        );
+        exec('npm -v', function (error, stdout, stderr) {
+            var versionNb = stdout.charAt(0);
+
+            if(versionNb < 3 ){
+                spawn(
+                    gulpExecPathOld,
+                    ['build:prod', '--cwd', cwd, '--color', 'always'],
+                    { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
+                )
+            } else {
+                spawn(
+                    gulpExecPathNew,
+                    ['build:prod', '--cwd', cwd, '--color', 'always'],
+                    { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
+                )
+            }
+        });
     })
     .command("watch", "build task, and automatically rebuild modified files", function (yargs) {
-        spawn(
-            gulpExecPath,
-            ['watch', '--cwd', cwd, '--color', 'always'],
-            { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
-        );
+        exec('npm -v', function (error, stdout, stderr) {
+            var versionNb = stdout.charAt(0);
+
+            if(versionNb < 3 ){
+                spawn(
+                    gulpExecPathOld,
+                    ['watch', '--cwd', cwd, '--color', 'always'],
+                    { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
+                )
+            } else {
+                spawn(
+                    gulpExecPathNew,
+                    ['watch', '--cwd', cwd, '--color', 'always'],
+                    { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
+                )
+            }
+        });
     })
     .command("live", "watch task, plus automatically reload browser", function (yargs) {
-        spawn(
-            gulpExecPath,
-            ['live', '--cwd', cwd, '--color', 'always'],
-            { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
-        );
+        exec('npm -v', function (error, stdout, stderr) {
+            var versionNb = stdout.charAt(0);
+
+            if(versionNb < 3 ){
+                spawn(
+                    gulpExecPathOld,
+                    ['live', '--cwd', cwd, '--color', 'always'],
+                    { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
+                )
+            } else {
+                spawn(
+                    gulpExecPathNew,
+                    ['live', '--cwd', cwd, '--color', 'always'],
+                    { env: {NODE_ENV: 'dev'}, stdio: 'inherit' }
+                )
+            }
+        });
     })
     .demandCommand(1, "Simply type 'dfb build' or whatever task you want to launch\n ")
     .help("h", "Display command list")
